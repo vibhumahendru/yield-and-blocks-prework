ddb7f359aba520557b8abd48fd45ae2932001684
vibhu mahendru <vibhumahendru@gmail.com>
2018-10-05 13:50:59 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,10 @@
-def hello_t(array)
-i = 0
-while i < array.length 
-yield array[i]
-i = i + 1
-end
-end
+#def hello_t(array)
+#i = 0
+#while i < array.length 
+#yield array[i]
+#i = i + 1
+#end
+#end
 
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
